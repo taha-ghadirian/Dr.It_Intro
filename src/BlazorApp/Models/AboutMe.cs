fb40ba0cd6a160a7a88e062a8f3ab66b6250c183
Taha Ghadirian <taha.ghadirian@outlook.com>
2023-03-14 08:36:51 +0000

    First commit for team preview

--- src/BlazorApp/Models/AboutMe.cs
+++ src/BlazorApp/Models/AboutMe.cs
@@ -3,6 +3,6 @@ namespace BlazorApp.Models;
 public class AboutMe
 {
     public string Description { get; set; } = string.Empty;
-    public List<string> Skills { get; set; } = new();
+    public List<string> Goals { get; set; } = new();
     public string DetailOrQuote { get; set; } = string.Empty;
 }
